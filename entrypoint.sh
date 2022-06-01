c7ad0a0dd2ce75d3f6145a6a45107e6cf39f5c21
Jim Brancheau <jim.brancheau@gmail.com>
2022-05-31 23:01:13 -0400

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ export NEW_RELIC_API_KEY=${NEW_RELIC_USER_API_KEY}
 export NEW_RELIC_REGION=${NEW_RELIC_REGION}
 export NEW_RELIC_LICENSE_KEY=${NEW_RELIC_INSERT_API_KEY}
 
+echo $NEW_RELIC_LICENSE_KEY
+
 result=$(newrelic reporting junit \
   --accountId "${NEW_RELIC_ACCOUNT_ID}" \
   --path "${NEW_RELIC_TEST_OUTPUT_PATH}" \

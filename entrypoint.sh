0dc2f248725edaa589937bfbef301c2274882fb9
Jim Brancheau <jim.brancheau@gmail.com>
2022-05-31 23:18:22 -0400

    remove some echo logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,6 @@ export NEW_RELIC_API_KEY=dummy_key
 export NEW_RELIC_REGION=${NEW_RELIC_REGION}
 export NEW_RELIC_LICENSE_KEY=${NEW_RELIC_INGEST_LICENSE_KEY}
 
-echo "IS THIS THING ON? $NEW_RELIC_LICENSE_KEY"
-
 result=$(newrelic reporting junit \
   --accountId "${NEW_RELIC_ACCOUNT_ID}" \
   --path "${NEW_RELIC_TEST_OUTPUT_PATH}" \
